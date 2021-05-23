751bf2aa0ad9679699e0f7c61a661f2e58684b31
adilosa95 <adilosa95@gmail.com>
2021-05-23 13:49:20 +0200

    CypressTest

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -1,6 +1,7 @@
 describe("The Home Page", () => {
   it("successfully loads", () => {
     cy.visit("/");
-    cy.findByRole("heading", { name: /hello world/i }).should("exist");
+    cy.findByRole("heading", { name: /Calculadora/i }).should("exist");
+    cy.findByRole("button", { name: /AC/i }).should("exist");
   });
 });
